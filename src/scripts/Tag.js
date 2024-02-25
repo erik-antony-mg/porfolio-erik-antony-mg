0b52dfe7b7ff62cccb7fd37c86f975aab5d240a6
erik antony <erik.antony,mg@gmail.com>
2024-02-25 09:22:03 -0500

    porfolio v1

---
+++ src/scripts/Tag.js
@@ -0,0 +1,138 @@
+
+import Angular from "../icons/Angular.astro";
+import Mysql from "../icons/Mysql.astro";
+import JWT from "../icons/JWT.astro";
+import Spring from "../icons/Spring.astro";
+import Git from "../icons/Git.astro";
+import Docker from "../icons/Docker.astro";
+import FrameAstro from "../icons/FrameAstro.astro";
+import GitHub from "../icons/GitHub.astro";
+import IntellijIdea from "../icons/IntellijIdea.astro";
+import Java from "../icons/Java.astro";
+import JavaScript from "../icons/JavaScript.astro";
+import Mongo from "../icons/Mongo.astro";
+import Postman from "../icons/Postman.astro";
+import TypeScript from "../icons/TypeScript.astro";
+import React from "../icons/React.astro";
+import Tailwind from "../icons/Tailwind.astro";
+import VisualStudio from "../icons/VisualStudio.astro";
+import Html from "../icons/Html.astro"
+import Css from "../icons/Css.astro";
+import PostGre from "../icons/PostGre.astro"
+import Swagger from "../icons/Swagger.astro"
+import Maven from "../icons/Maven.astro";
+const TAGS = {
+  MAVEN: {
+    nombre: "Maven",
+    class: "",
+    icono: Maven,
+  },
+    SWAGGER: {
+      nombre: "Swagger",
+      class: "",
+      icono: Swagger,
+    },
+    POSTGRESQL: {
+      nombre: "PostgreSQL",
+      class: "",
+      icono: PostGre,
+    },
+    CSS: {
+      nombre: "Css",
+      class: "",
+      icono: Css,
+    },
+    HTML: {
+      nombre: "Html",
+      class: "",
+      icono: Html,
+    },
+      VISUALSTUDIO: {
+      nombre: "V S C",
+      class: "",
+      icono: VisualStudio,
+    },
+      TAILWIND: {
+      nombre: "Tailwind",
+      class: "",
+      icono: Tailwind,
+    },
+      REACT: {
+      nombre: "React",
+      class: "",
+      icono: React,
+    },
+      TYPESCREIPT: {
+      nombre: "TypeScript",
+      class: "",
+      icono: TypeScript,
+    },
+    POSTMAN: {
+      nombre: "Postman",
+      class: "",
+      icono: Postman,
+    },
+    MONGO: {
+      nombre: "Mongo",
+      class: "",
+      icono: Mongo,
+    },
+    JAVASCRIPT: {
+      nombre: "JavaScript",
+      class: "",
+      icono: JavaScript,
+    },
+    JAVA: {
+      nombre: "Java",
+      class: "",
+      icono: Java,
+    },
+    INTELLIJIDEA: {
+      nombre: "IntellijIdea",
+      class: "",
+      icono: IntellijIdea,
+    },
+    GITHUB: {
+      nombre: "GitHub",
+      class: "",
+      icono: GitHub,
+    },
+    FRAMEASTRO: {
+      nombre: "Astro",
+      class: "",
+      icono: FrameAstro,
+    },
+    DOCKER: {
+      nombre: "Docker",
+      class: "",
+      icono: Docker,
+    },
+    ANGULAR: {
+      nombre: "Angular",
+      class: "",
+      icono: Angular,
+    },
+  
+    MYSQL: {
+      nombre: "Mysql",
+      class: "",
+      icono: Mysql,
+    },
+    JWT: {
+      nombre: "JWT",
+      class: "",
+      icono: JWT,
+    },
+    SPRING: {
+      nombre: "Spring",
+      class: "",
+      icono: Spring,
+    },
+    GIT: {
+      nombre: "Git",
+      class: "",
+      icono: Git,
+    },
+  };
+
+  export {TAGS}
